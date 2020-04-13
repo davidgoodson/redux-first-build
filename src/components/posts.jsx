f61d45b1c4aa336f67e599e1a1dba38d3a7cea16
David Buyinza <davidbuyinza256@gmail.com>
2020-04-13 15:28:51 +0300

    Setup Redux and Render Posts

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import connect from "react-redux";
+import { fetchPosts } from "../redux/actions/postActions";
+import PropTypes from "prop-types";
 
 class Posts extends Component {
-  state = {};
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.newPost) this.props.posts.unshift(newProps.newPost);
+  }
+
   render() {
     return (
       <div>
-        {this.state.posts.map((post) => (
+        {this.props.posts.map((post) => (
           <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
@@ -16,4 +30,15 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  newPOst: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+  newPOst: state.posts.item,
+});
+
+export default connect(mapStateToProps, fetchPosts)(Posts);
